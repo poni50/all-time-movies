3c579199eb9f2881f7a401ddbecdd711af39cd53
Prodan Georgiev <proboy599@gmail.com>
2020-10-23 15:02:34 +0200

    tiempo

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,7 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(array) {
+    return array.map((element) => {
+        element.duration = getCorrectTime(element.duration);
+    });
+}
 
+function getCorrectTime(str) {
 
+    if (str.includes("h")) {
+        if (str.includes("min")) {
+            let strings = str.split("h");
+            let horas = parseInt(strings[0]) * 60;
+            let minutes = strings[1].split("min")[0].trim();
+            let result = horas + parseInt(minutes);
+            return result + "min";
+        }
+
+        let strings = str.split("h");
+        let horas = parseInt(strings[0]) * 60;
+        return horas + "min";
+    }
+    return str;
+}
 // Get the average of all rates with 2 decimals 
 
 
